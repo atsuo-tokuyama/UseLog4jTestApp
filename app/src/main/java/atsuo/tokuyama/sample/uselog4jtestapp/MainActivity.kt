76f4862498617952d0d526cedf08b32b2b87dba4
Atsuo Tokuyama <atsuo.tokuyama@westunitis.co.jp>
2021-12-16 17:44:07 +0900

    Initial commit

---
+++ app/src/main/java/atsuo/tokuyama/sample/uselog4jtestapp/MainActivity.kt
@@ -0,0 +1,13 @@
+package atsuo.tokuyama.sample.uselog4jtestapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        atsuo.tokuyama.sample.uselog4jlib.print()
+    }
+}
---
+++ useLog4jLib/src/main/java/atsuo/tokuyama/sample/uselog4jlib/Log4jSample.kt
@@ -0,0 +1,8 @@
+package atsuo.tokuyama.sample.uselog4jlib
+
+import android.util.Log
+import org.apache.logging.log4j.EventLogger
+
+fun print() {
+    Log.i("uselog4jlib", "I use log4j.")
+}
